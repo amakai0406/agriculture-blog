3cd4f56a689d33f1b9232f64518f562f777ec288
あまかい0406 <amakaiamakai0406@gmail.com>
2024-07-09 23:57:22 +0900

    Web.phpコミット忘れ

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\DashboardController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/admin', [DashboardController::class, 'index']);
+
