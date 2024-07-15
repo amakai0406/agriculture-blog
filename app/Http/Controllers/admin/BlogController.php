077e583667a266d8f62fe5f96ac20d52fc9d7162
あまかい0406 <amakaiamakai0406@gmail.com>
2024-07-15 17:37:19 +0900

    DBよりデータ取得し、表示

--- app/Http/Controllers/admin/BlogController.php
+++ app/Http/Controllers/admin/BlogController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers\admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Blog;
 
 class BlogController extends Controller
 {
 
     public function index()
     {
-        return view('admin.blogs.index');
+        $blogs = Blog::latest('created_at')->simplePaginate(10);
+        return view('admin.blogs.index', compact('blogs'));
     }
 }
--- resources/views/admin/blogs/index.blade.php
+++ resources/views/admin/blogs/index.blade.php
@@ -8,7 +8,30 @@
 </head>
 
 <body>
-    <h2>ブログ一覧</h2>
+    <div class="container mt-5">
+        <h1 class="mb-4">Blog List</h1>
+        <div class="row">
+            @foreach ($blogs as $blog)
+                <div class="col-md-6 col-lg-4">
+                    <div class="card blog-card">
+                        <img src="{{ $blog->image }}" class="card-img-top" alt="{{ $blog->title }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $blog->title }}</h5>
+                            <p class="card-text">{{ \Illuminate\Support\Str::limit($blog->content, 100) }}</p>
+                            <p class="card-text"><small class="text-muted"></small></p>
+                            <form action="" method="POST" style="display:inline;">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">削除</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+        {{ $blogs->links() }}
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
