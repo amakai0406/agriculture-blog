86efbe044b18d4a5236f5869cb09e8d8d922d5e0
あまかい0406 <amakaiamakai0406@gmail.com>
2024-07-16 17:25:05 +0900

    コメントの追加

--- app/Http/Controllers/admin/BlogController.php
+++ app/Http/Controllers/admin/BlogController.php
@@ -10,7 +10,9 @@ class BlogController extends Controller
 
     public function index()
     {
+        //blogsテーブルから取得したデータをlatestメソッドでcreated_atの新しい順に1ページ１０件でページングするし、$blogsに格納
         $blogs = Blog::latest('created_at')->simplePaginate(10);
+        //compactメソッドで$blogsをビューに渡し、admin.blogs.indexビューを返す
         return view('admin.blogs.index', compact('blogs'));
     }
 }
--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -10,9 +10,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blog extends Model
 {
-    //Authenticatable(承認システム機能)追加
     use HasFactory;
 
+    //'title', 'content', 'image', 'created_at',は、フォームからのデータを直接Blogモデルに振り当てるように設定している
     protected $fillable = ['title', 'content', 'image', 'created_at'];
 
 }
