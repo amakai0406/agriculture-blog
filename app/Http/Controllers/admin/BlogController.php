984df11b67f87e8f4cc48451b4a1c15be10242f9
あまかい0406 <amakaiamakai0406@gmail.com>
2024-07-17 12:13:35 +0900

    コメントの追加

--- app/Http/Controllers/admin/BlogController.php
+++ app/Http/Controllers/admin/BlogController.php
@@ -19,27 +19,35 @@ class BlogController extends Controller
 
     public function create()
     {
-
+        //ブログ作成ブログ作成ページを返す
         return view('admin.blogs.create');
     }
 
     public function store(StoreBlogRequest $request)
     {
 
+        //HttpリクエストデータをStoreBlogRequestで設定したルールに基に検証し、検証が成功したデータを$validatedに格納する
         $validated = $request->validated();
 
+        //Httpリクエストデータの中にhasFileメソッドでimageファイルがあるか確認し、確認できた場合
         if ($request->hasFile('image')) {
 
+            //imageファイルを$imageに格納する
             $image = $request->file('image');
 
+            //$imageをstoreメソッドでstorage/app/public/imagesに保存し、そのパスを$pathに格納する
             $path = $image->store('public/images');
 
+            //$pathをbasenameメソッドを使い、パスのファイル名だけを$imageNameに格納する
             $imageName = basename($path);
 
+            //パスのファイル名を検証されたimageファイル$validated['image']に格納する
             $validated['image'] = $imageName;
 
+            //Blogモデルを使い、検証済みデータの$validatedを基にインスタンスを作成する
             Blog::create($validated);
 
+            //admin.blogs.indexにリダイレクトし、登録成功後に新しいブログを投稿しましたとメッセージを表示する
             return to_route('admin.blogs.index')->with('success', '新しいブログを投稿しました');
 
         }
--- app/Http/Requests/admin/StoreBlogRequest.php
+++ app/Http/Requests/admin/StoreBlogRequest.php
@@ -8,6 +8,7 @@ class StoreBlogRequest extends FormRequest
 {
     public function authorize()
     {
+        //リクエストを許可している
         return true;
     }
 
@@ -15,8 +16,11 @@ class StoreBlogRequest extends FormRequest
     {
 
         return [
+            //required(必須)、string(文字列)、max:255(最大255文字)
             'title' => 'required|string|max:255',
-            'content' => 'required|string|',
+            //required(必須)、string(文字列)、max:5000(最大5000文字)
+            'content' => 'required|string|max:5000',
+            //required(必須)、image(画像ファイル)、mimes:jpeg,png,jpg,gif(画像ファイルの形式指定)、max:2048(最大2048キロバイト（2MB）)
             'image' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
         ];
     }
