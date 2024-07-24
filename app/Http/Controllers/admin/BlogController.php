1058c5ce005198ad3e0e3598c497e9ad75b5eb24
あまかい0406 <amakaiamakai0406@gmail.com>
2024-07-24 23:01:01 +0900

    コード修正

--- app/Http/Controllers/admin/BlogController.php
+++ app/Http/Controllers/admin/BlogController.php
@@ -11,7 +11,7 @@ class BlogController extends Controller
     public function index()
     {
         //blogsテーブルから取得したデータをlatestメソッドでcreated_atの新しい順に1ページ１０件でページングし、$blogsに格納
-        $blogs = Blog::latest('created_at')->with('images')->simplePaginate(10);
+        $blogs = Blog::latest('created_at')->simplePaginate(10);
         //compactメソッドで$blogsをビューに渡し、admin.blogs.indexビューを返す
         return view('admin.blogs.index', compact('blogs'));
     }
