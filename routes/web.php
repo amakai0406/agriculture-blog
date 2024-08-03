8303c4fdf32020ed578c5a67d58eb7e03059c6a5
あまかい0406 <amakaiamakai0406@gmail.com>
2024-08-03 10:50:59 +0900

    コメントの編集

--- routes/web.php
+++ routes/web.php
@@ -14,68 +14,67 @@ use App\Models\Admin;
 Route::get('/admin/admins/create', [AdminController::class, 'create'])->name('admin.admins.create');
 Route::post('/admin/admins', [AdminController::class, 'store'])->name('admin.admins.store');
 
-//ログインページの表示
+//ログインページ
 Route::get('/admin/admins/login', [AuthController::class, 'index'])->name('admin.admins.login');
+
 //ログイン処理
 Route::post('/admin/admins/login', [AuthController::class, 'login']);
 
-//野菜一覧ページの表示
+//やさい一覧ページ
 Route::get('/user/vegetables', [UserVegetableController::class, 'index'])->name('user.vegetables.index');
 
-//野菜の詳細ページの表示
+//やさい詳細ページ
 Route::get('/user/vegetables/{id}', [UserVegetableController::class, 'show'])->name('user.vegetables.show');
 
-//ブログ一覧ページの表示
+//ブログ一覧ページ
 Route::get('/user/blogs', [UserBlogController::class, 'index'])->name('user.blogs.index');
 
-//ブログの詳細ページの表示
+//ブログ詳細ページ
 Route::get('/user/blogs/{id}', [UserBlogController::class, 'show'])->name('user.blogs.show');
 
 //ログイン承認後ルート
 Route::middleware([Authenticate::class])->group(function () {
 
-    //ダッシュボードの表示
+    //ダッシュボード
     Route::get('/admin/dashboard', [DashboardController::class, 'index'])->name('admin.dashboard');
 
     //ログアウト
     Route::post('admin/logout', [AuthController::class, 'logout'])->name('admin.admins.logout');
 
-
-
-    //ブログ作成画面の表示
+    //ブログ作成ページ
     Route::get('/admin/blogs/create', [AdminBlogController::class, 'create'])->name('admin.blogs.create');
 
-    //ブログの投稿
+    //ブログ投稿
     Route::post('/admin/blogs', [AdminBlogController::class, 'store'])->name('admin.blogs.store');
 
-    //ブログ一覧の表示
+    //ブログ一覧ページ
     Route::get('/admin/blogs', [AdminBlogController::class, 'index'])->name('admin.blogs.index');
 
-    //ブログ編集
+    //ブログ編集ページ
     Route::get('/admin/blogs/{id}/edit', [AdminBlogController::class, 'edit'])->name('admin.blogs.edit');
 
-    //ブログの更新
+    //ブログ更新
     Route::put('/admin/blogs/{id}', [AdminBlogController::class, 'update'])->name('admin.blogs.update');
 
-    //ブログの削除
+    //ブログ削除
     Route::delete('admin/blogs/{id}', [AdminBlogController::class, 'destroy'])->name('admin.blogs.destroy');
 
-
-    //やさい一覧ページの表示
+    //やさい一覧ページ
     Route::get('/admin/vegetables', [AdminVegetableController::class, 'index'])->name('admin.vegetables.index');
 
-    //やさいの追加ページの表示
+    //やさい追加ページ
     Route::get('/admin/vegetables/create', [AdminVegetableController::class, 'create'])->name('admin.vegetables.create');
 
-    //やさいの登録処理
+    //やさい登録
     Route::post('/admin/vegetables', [AdminVegetableController::class, 'store'])->name('admin.vegetables.store');
 
-    //やさいの詳細ページの表示
+    //やさい編集ページ
     Route::get('/admin/vegetables/{id}/edit', [AdminVegetableController::class, 'edit'])->name('admin.vegetables.edit');
 
-    //野菜の野菜の更新
+    //やさい更新
     Route::put('/admin.vegetables/{id}', [AdminVegetableController::class, 'update'])->name('admin.vegetables.update');
 
+    //やさい削除
     Route::delete('/admin/vegetables/{id}', [AdminVegetableController::class, 'destroy'])->name('admin.vegetables.destroy');
 
 });
