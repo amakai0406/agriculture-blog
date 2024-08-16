7fa6ecb6ab2ebde93fde49454fd9bad3ffea7ef9
あまかい0406 <amakaiamakai0406@gmail.com>
2024-08-16 23:16:45 +0900

    コード修正

--- resources/views/user/blogs/index.blade.php
+++ resources/views/user/blogs/index.blade.php
@@ -5,23 +5,22 @@
 @section('content')
 <link rel="stylesheet" href="{{ asset('css/blog-index.css') }}">
 
-<h1>ブログ一覧</h1>
+<h1 class="centered-title">ブログ一覧</h1>
 <div class="blog-list">
     @foreach($blogs as $blog)
-        <a href="{{ route('user.blogs.show', ['id' => $blog->id])}}" class="blog-Link">
-            <div class="blog-item">
-                <h2>{{ $blog->title }}</h2>
-                <div class="blog-images">
-                    @foreach($blog->images as $image)
-                        <img src="{{ asset('storage/' . $image->image_path) }}" alt="Blog Image">
-                    @endforeach
-                </div>
-                <p>{{ \Illuminate\Support\Str::limit($blog->content, 100, '...') }}</p>
-                <div>
-                    {{ $blog->created_at->format('Y-m-d H:i') }}
-                </div>
+        <div class="blog-item">
+            <h2>{{ $blog->title }}</h2>
+            <div class="blog-images">
+                @foreach($blog->images as $image)
+                    <img src="{{ asset('storage/' . $image->image_path) }}" alt="Blog Image">
+                @endforeach
             </div>
-        </a>
+            <p>{{ \Illuminate\Support\Str::limit($blog->content, 100, '...') }}</p>
+            <a href="{{ route('user.blogs.show', ['id' => $blog->id])}}" class="blog-link">このブログの詳細へ</a>
+            <div>
+                {{ $blog->created_at->format('Y-m-d H:i') }}
+            </div>
+        </div>
     @endforeach
 </div>
 <!-- ページネーション -->
